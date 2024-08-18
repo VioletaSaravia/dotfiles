9bf01d445427d298fc26af11b7741cc830a55d62
Violeta Saravia <saraviavioleta@gmail.com>
2024-08-18 13:14:17 -0300

    2024-08-18

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,3 +7,7 @@ sudo yay -R wofi
 sudo systemctl enable tlp.service
 dmenu-emoji | dmenu | awk {print $1} | tr -d '\n' | wl-copy && wl-paste
 bluetoothctl -- devices | dmenu | awk '{print $2}' | xargs bluetoothctl connect
+python -c 'print(0x1234)'
+echo $((16#1234))
+iwctl -- station wlan0 get-networks | tail -n +5 | cut -c1-36 | dmenu | xargs iwctl -- station wlan0 connect
+#Bash func that takes regex, removes spaces other than escaped ones
